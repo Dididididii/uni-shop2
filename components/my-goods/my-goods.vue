a2b261247d0f01b8597d339431d48b18e312a2fd
Dididididii <1281627297@qq.com>
2022-05-08 21:38:02 +0800

    完成了购物车的开发

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -2,6 +2,7 @@
   <view class="goods-item">
     <!-- 商品左侧图片区域 -->
     <view class="goods-item-left">
+      <radio :checked="goods.goods_state" color="#C00000" v-if="showRadio" @click="radioClickHandler"></radio>
       <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
     </view>
     <!-- 商品右侧信息区域 -->
@@ -11,6 +12,8 @@
       <view class="goods-info-box">
         <!-- 商品价格 -->
         <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+        <!-- 商品数量 -->
+        <uni-number-box :min="1" :value="goods.goods_count" v-if="showNum" @change="numChangeHandler"></uni-number-box>
       </view>
     </view>
   </view>
@@ -23,7 +26,18 @@
       goods:{
         type:Object,
         default:{}
-      }
+      },
+      // 是否展示图片左侧的 radio
+          showRadio: {
+            type: Boolean,
+            // 如果外界没有指定 show-radio 属性的值，则默认不展示 radio 组件
+            default: false,
+          },
+          // 是否展示价格右侧的 NumberBox 组件
+              showNum: {
+                type: Boolean,
+                default: false,
+              },
     },
     data() {
       return {
@@ -35,19 +49,39 @@
       tofixed(num){
         return Number(num).toFixed(2)
       }
+    },
+    methods:{
+      radioClickHandler(){
+        this.$emit('radio-change',{
+          goods_id:this.goods.goods_id,
+          goods_state: !this.goods.goods_state
+        })
+      },
+      numChangeHandler(val){
+        this.$emit('num-change', {
+          goods_id:this.goods.goods_id,
+          goods_count:+val
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
 .goods-item {
+  // 让 goods-item 项占满整个屏幕的宽度
+    width: 750rpx;
+    // 设置盒模型为 border-box
+    box-sizing: border-box;
   display: flex;
   padding: 10px 5px;
   border-bottom: 1px solid #f0f0f0;
 
   .goods-item-left {
     margin-right: 5px;
-
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     .goods-pic {
       width: 100px;
       height: 100px;
@@ -57,13 +91,18 @@
 
   .goods-item-right {
     display: flex;
+    flex: 1;
     flex-direction: column;
     justify-content: space-between;
 
     .goods-name {
       font-size: 13px;
     }
-
+    .goods-info-box {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
     .goods-price {
       font-size: 16px;
       color: #c00000;
